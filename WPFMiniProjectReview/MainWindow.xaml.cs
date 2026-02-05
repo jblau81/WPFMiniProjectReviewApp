48bb6b0aa0dbd543faf797fced85040b4df17a99
Josh <jblau2017@yahoo.com>
2026-02-05 09:38:47 -0700

    Add project files.

---
+++ WPFMiniProjectReview/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DemoLibrary;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMiniProjectReview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addressesList.ItemSource = addresses;
+
+            addresses.Add(new AddressModel
+            {
+                StreetAddress = "123 Wallabe Way",
+                City = "Clarks Summit",
+                State = "PA",
+                ZipCode = "18411"
+            });
+        }
+    }
+}
